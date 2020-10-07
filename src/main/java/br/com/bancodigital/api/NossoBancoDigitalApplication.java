b769d0505e22c19bf405cbe88da2170f364e2c9d
Gilson Silva <gilsonsilvati@gmail.com>
2020-10-06 23:00:59 -0300

    Criando o projeto

---
+++ src/main/java/br/com/bancodigital/api/NossoBancoDigitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.bancodigital.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NossoBancoDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NossoBancoDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bancodigital/api/NossoBancoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bancodigital.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NossoBancoDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
